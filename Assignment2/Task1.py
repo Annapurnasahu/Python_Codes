dd7b2724b6d6070460204aacb9b74146597137eb
annapurna sahu <girlannusahu675@gmail.com>
2025-11-02 22:21:31 +0530

    Assignment2 Task1 Task2

---
+++ Assignment2/Task1.py
@@ -0,0 +1,8 @@
+# Task: Check if a Number is Even or Odd
+# Author: Annapurna Sahu
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("The number is Even.")
+else:
+    print("The number is Odd.")
---
+++ Assignment2/Task2.py
@@ -0,0 +1,8 @@
+# Task: Sum of Integers from 1 to 50 Using a Loop
+# Author: Annapurna Sahu
+
+total = 0
+for i in range(1, 51):
+    total += i
+
+print("The sum of numbers from 1 to 50 is:", total)
