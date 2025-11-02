4f9e1a189c76b0a0a80f197cae861b78ea89f3b7
annapurna sahu <girlannusahu675@gmail.com>
2025-11-02 22:10:15 +0530

    Assignment1 Task1 Task2

---
+++ Assignment1/Task1/Task1.py
@@ -0,0 +1,19 @@
+# author: annapurna sahu
+# Task 1: Perform Basic Mathematical Operations
+
+# Taking two numbers as input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Performing basic mathematical operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Displaying the results
+print("\n--- Results ---")
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
---
+++ Assignment1/Task2/Task2.py
@@ -0,0 +1,12 @@
+# Task 2: Create a Personalized Greeting
+# Author: Annapurna Sahu
+
+# Taking user's first and last name as input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenating first name and last name
+full_name = first_name + " " + last_name
+
+# Printing personalized greeting message
+print("Hello,", full_name + "! Welcome to Python programming.")
