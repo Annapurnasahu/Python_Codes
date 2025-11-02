7a441ec66ee9304a3f08de70c9c0c4c18a7ecbdf
annapurna sahu <girlannusahu675@gmail.com>
2025-11-02 22:29:46 +0530

    Assignment3 Task1 Task2

---
+++ Assignment3/Task2.py
@@ -0,0 +1,14 @@
+# Task: Calculate Square Root, Log, and Sine
+# Author: Annapurna Sahu
+
+import math
+
+num = float(input("Enter a number: "))
+
+sqrt_val = math.sqrt(num)
+log_val = math.log(num)
+sine_val = math.sin(num)
+
+print("Square root: ", num, "is:", sqrt_val)
+print("Logarithm: ", num, "is:", log_val)
+print("Sine: ", num, "is:", sine_val)
